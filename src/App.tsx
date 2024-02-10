0cfa35eed9b1ba6094b8f384f15fe89807d719f4
Askhad Arzybaev <aarzybaev@gmail.com>
2024-02-10 09:21:46 +0600

    wrote routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import Appbar from './components/Appbar/Appbar';
+import {Route, Routes} from 'react-router-dom';
+import Posts from './containers/Posts/Posts';
+import NewPost from './containers/NewPost/NewPost';
+import Post from './containers/Post/Post';
+import About from './containers/About/About';
+import Contacts from './containers/Contacts/Contacts';
 
 const App = () => (
   <>
-    <h3>Hello</h3>
+    <header>
+      <Appbar/>
+    </header>
+    <main className="container-fluid">
+      <Routes>
+        <Route path="/" element={<Posts/>} />
+        <Route path="/new-post" element={<NewPost/>} />
+        <Route path="/posts/:id" element={<Post/>}/>
+        <Route path="/posts/:id/edit" element={<NewPost/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contacts" element={<Contacts/>}/>
+        <Route path="*" element={<h1>Not found</h1>} />
+      </Routes>
+    </main>
   </>
 );
 
